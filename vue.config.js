1ce56b23a24ef440f3701504580a21697f4378a0
Artjom Löbsack <me@ceigh.com>
2021-07-10 16:40:22 +0500

    feat: inline js in html

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+
 module.exports = {
+  productionSourceMap: false,
+
   pluginOptions: {
     lintStyleOnBuild: true
+  },
+
+  css: {
+    extract: false
+  },
+
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.html',
+        inlineSource: '.(js|css)$'
+      }),
+      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+    ]
   }
 }
