1e2a6b9fb421f183f3f6e72914e4b3cb5f0a5205
Artjom Löbsack <me@ceigh.com>
2021-07-10 16:16:37 +0500

    feat: add fonts

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,17 @@
 </template>
 
 <script lang="ts">
+import '@fontsource/rubik'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
 })
 </script>
+
+<style lang="scss">
+@import 'modern-css-reset';
+
+body {
+  font-family: Rubik, sans-serif;
+}
+</style>
