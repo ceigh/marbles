68c58c7cb95798525b637fb89e4c718280faa6d8
Artjom Löbsack <me@ceigh.com>
2021-07-14 17:06:58 +0500

    feat(screen 8): add buttons

--- src/components/screen/Screen8.vue
+++ src/components/screen/Screen8.vue
@@ -6,11 +6,53 @@
     </h4>
 
     <div class="bottom">
-      <div class="links" />
-      <p>г. Москва, ул. Мясницкая 13с18, здание с вывеской ФРИИ, Марблс</p>
+      <div>
+        <button class="tg">
+          <a
+            :href="`https://t.me/${tg}`"
+            target="_blank"
+          >
+            <img
+              :src="`${assetsPath}img/telegram.svg`"
+              alt="telegram"
+            >
+            <span>@{{ tg }}</span>
+          </a>
+        </button>
+
+        <button class="email">
+          <a
+            :href="`mailto:${email}`"
+          >
+            <img
+              :src="`${assetsPath}img/email.svg`"
+              alt="email"
+            >
+            <span>{{ email }}</span>
+          </a>
+        </button>
+      </div>
+
+      <div>
+        <p>г. Москва, ул. Мясницкая 13с18, здание с вывеской ФРИИ, Марблс</p>
+      </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      assetsPath: process.env.VUE_APP_ASSETS,
+      tg: 'nikolayr',
+      email: 'help@marbls.ru'
+    }
+  }
+})
+</script>
 
 <style lang="scss" scoped>
 .screen {
@@ -32,6 +74,55 @@
 
   p {
     max-width: 380px;
+    color: $text-gray;
+  }
+}
+
+button {
+  background: transparent;
+  border: 1px solid;
+  border-radius: 26px;
+  border-color: var(--color);
+  color: var(--color);
+  padding: 0;
+
+  @include transition;
+
+  &:hover {
+    color: $white;
+    background: var(--color);
+  }
+
+  &:active {
+    background: var(--color-dark);
+  }
+
+  &:not(:last-child) {
+    margin-right: 40px;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    padding: 10px 12px;
+    font-size: 20px;
+    line-height: 30px;
+
+    img {
+      margin-right: 9px;
+    }
+  }
+
+  &.tg {
+    --color: #40b3e0;
+    --color-dark: #3497be;
+  }
+
+  &.email {
+    --color: #840707;
+    --color-dark: #600101;
   }
 }
 </style>
