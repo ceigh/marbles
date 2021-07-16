3219d44c1d9d7261f2b2a3de94030d254168302a
Artjom Löbsack <me@ceigh.com>
2021-07-16 16:31:52 +0500

    refactor: move hero to component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <app-header />
 
-  <img
-    class="photo"
-    :src="`${assetsPath}img/photo.jpeg`"
-    alt="фото"
-  >
+  <app-hero />
 
   <screen-1 />
   <screen-2 />
@@ -22,6 +18,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import AppHeader from './components/app/AppHeader.vue'
+import AppHero from './components/app/AppHero.vue'
 import Screen1 from './components/screen/Screen1.vue'
 import Screen2 from './components/screen/Screen2.vue'
 import Screen3 from './components/screen/Screen3.vue'
@@ -35,6 +32,7 @@ import AppFooter from './components/app/AppFooter.vue'
 export default defineComponent({
   components: {
     AppHeader,
+    AppHero,
     Screen1,
     Screen2,
     Screen3,
@@ -44,26 +42,10 @@ export default defineComponent({
     Screen7,
     Screen8,
     AppFooter
-  },
-
-  setup () {
-    const assetsPath = process.env.VUE_APP_ASSETS
-
-    return {
-      assetsPath
-    }
   }
 })
 </script>
 
 <style lang="scss">
 @import './assets/styles';
-</style>
-
-<style lang="scss" scoped>
-.photo {
-  width: 100%;
-  max-height: 546px;
-  object-fit: cover;
-}
 </style>
---
+++ src/components/app/AppHero.vue
@@ -0,0 +1,29 @@
+<template>
+  <img
+    class="photo"
+    :src="`${assetsPath}img/photo.jpeg`"
+    alt="фото"
+  >
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const assetsPath = process.env.VUE_APP_ASSETS
+
+    return {
+      assetsPath
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.photo {
+  width: 100%;
+  max-height: 546px;
+  object-fit: cover;
+}
+</style>
