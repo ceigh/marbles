1be3f1f278baf1de413f80fd8fef7d85c9cf11a6
Artjom Löbsack <me@ceigh.com>
2021-07-16 16:57:30 +0500

    feat(hero): add video

--- src/components/app/AppHero.vue
+++ src/components/app/AppHero.vue
@@ -1,9 +1,19 @@
 <template>
-  <img
-    class="photo"
-    :src="`${assetsPath}img/photo.jpeg`"
-    alt="фото"
+  <video
+    autoplay
+    muted
+    loop
+    :poster="`${assetsPath}img/photo.jpeg`"
   >
+    <source
+      src="https://cdn.shopify.com/s/files/1/0312/2737/9849/files/HomeHero_20210120.webm"
+      type="video/webm"
+    >
+    <source
+      src="https://cdn.shopify.com/s/files/1/0312/2737/9849/files/HomeHero_20210120.mp4"
+      type="video/mp4"
+    >
+  </video>
 </template>
 
 <script lang="ts">
@@ -21,9 +31,10 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.photo {
+video {
   width: 100%;
   max-height: 546px;
   object-fit: cover;
+  pointer-events: none;
 }
 </style>
